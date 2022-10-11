19aa3337d2dec9250614d30daee98a1c44cb5f2b
tejas <tejasshah@gmail.com>
2022-10-11 18:05:29 +0530

    first printf

---
+++ FIRST.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main(){
+	clrscr();//clear screen
+	printf("Royal"); // print something on console
+	getch();//console hold - key press - console hold
+}
