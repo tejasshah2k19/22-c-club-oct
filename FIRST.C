842d1b6f90d2cc5558af14a7a83889977027dc41
tejas <tejasshah@gmail.com>
2022-10-11 19:00:51 +0530

    printf demo and int data type

--- FIRST.C
+++ FIRST.C
@@ -6,3 +6,11 @@ void main(){
 	printf("Royal"); // print something on console
 	getch();//console hold - key press - console hold
 }
+
+//function ->
+// collection of instructions - set of instructions -> name ->
+
+//standard input output . header
+//console input output . header
+
+//photoshop   mspaint
